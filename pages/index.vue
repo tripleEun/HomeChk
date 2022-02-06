73d499068418ff36ebe0a0dc22fc8779fa1ef372
nomi <93440858+tripleEun@users.noreply.github.com>
2022-02-06 16:20:01 +0900

    nuxt setting

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- <h2>권노미에용ㅇㅅㅇ</h2> -->
+  <Tutorial />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ pages/main.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>권노미에용ㅇㅅㅇ</h2>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
